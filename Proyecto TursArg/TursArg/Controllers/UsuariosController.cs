4fe8fbc6198a05785f96b65f98a1b1aff55355c1
= <=>
2021-03-10 15:25:21 -0300

    Proyecto base Sprint 1

---
+++ Proyecto TursArg/TursArg/Controllers/UsuariosController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TursArg.Models;
+
+namespace TursArg.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // creo variable de tipo base de datos con terminación entities
+        BDTursArgEntities bd = new BDTursArgEntities();
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            List<Usuarios> listaUsuarios = bd.Usuarios.ToList();
+            return View(listaUsuarios);
+
+        }
+        // Registrar usaruarios hice el action de tipo get porque no me andaba el mismo metodo pero solo
+        // Post, asi que lo sobrecargue.
+        public ActionResult registrarUsuario()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult registrarUsuario(Usuarios usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            usuario.rolAdmin = false;
+            bd.Usuarios.Add(usuario);
+            bd.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
